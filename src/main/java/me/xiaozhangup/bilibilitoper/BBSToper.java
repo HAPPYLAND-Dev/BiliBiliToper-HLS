5d776c4fcd4bb8efa51d0432e0a747bf94cdf83f
xiaozhangup <68374769+xiaozhangup@users.noreply.github.com>
2022-10-05 20:48:03 +0800

    改了个材质

--- src/main/java/me/xiaozhangup/bilibilitoper/BBSToper.java
+++ src/main/java/me/xiaozhangup/bilibilitoper/BBSToper.java
@@ -15,8 +15,8 @@ import static me.xiaozhangup.bilibilitoper.BiliBiliToper.mm;
 
 public class BBSToper implements Listener, InventoryHolder {
 
-    public static final ItemStack bbs = IBuilder.buildItem(Material.FIREWORK_ROCKET, "&cMCBBS使用提升卡顶贴", "", "&7这一项适用于拥有MCBBS账号", "&7且账号等级大于4级的玩家使用");
-    public static final ItemStack bili = IBuilder.buildItem(Material.LINGERING_POTION, "&x&0&1&a&1&d&6哔哩哔哩实况", "", "&7你可以录制服务器的游玩实况", "&7并发布到BiliBili,即可获取奖励");
+    public static final ItemStack bbs = IBuilder.buildItem(Material.BROWN_DYE, "&x&c&a&8&1&4&cMCBBS使用提升卡顶贴", "", "&7这一项适用于拥有MCBBS账号", "&7且账号等级大于4级的玩家使用");
+    public static final ItemStack bili = IBuilder.buildItem(Material.BLUE_DYE, "&x&0&1&a&1&d&6哔哩哔哩实况", "", "&7你可以录制服务器的游玩实况", "&7并发布到BiliBili,即可获取奖励");
     private static final ItemStack board = IBuilder.getBorder(Material.GRAY_STAINED_GLASS_PANE);
     public static Inventory bbstoper = Bukkit.createInventory(new BBSToper(), 27, mm.deserialize("你想如何支持我们?"));
 
